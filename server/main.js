c0e05fe37cafd8cdac204ffb74c017b3d400eb4d
Bob Wang <wangbo@yiqijiao.cn>
2018-12-29 11:54:34 +0800

    save

--- server/main.js
+++ server/main.js
@@ -2,6 +2,9 @@ import {
   Meteor
 } from 'meteor/meteor';
 
+import Redis from 'ioredis';
+const redis = new Redis();
+
 // 导入模块的写法
 // import {
 //   Doctor
@@ -10,4 +13,7 @@ import {
 Meteor.startup(() => {
   // code to run on server at startup
   console.error(Doctor);
+
+  Promise.await(redis.set('key', 'value'));
+  console.error(Promise.await(redis.get('key')));
 });
